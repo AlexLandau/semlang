f2c3eeeba696cbf3e591ec992a80100f0c92aa10
alandau <alandau@cs.stanford.edu>
2016-10-05 13:20:50 +0100

    Add some Kotlin stuff.

---
+++ java-interpreter/src/main/java/semlang/Interpreter.java
@@ -0,0 +1,9 @@
+package semlang;
+import java.util.List;
+
+public class Interpreter {
+    //TODO: Deal with a code source as something that can
+    public void interpret(CodeSource source, FunctionId id, List<Object> javaArguments) {
+        throw new UnsupportedOperationException();
+    }
+}
