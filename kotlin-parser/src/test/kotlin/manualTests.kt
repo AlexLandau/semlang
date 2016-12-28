d4899d36513c6bec4cd6011733e243dc1ba570c5
alandau <alandau@cs.stanford.edu>
2016-12-27 16:08:38 -0800

    Clean up the test code a bit more.

--- kotlin-parser/src/test/kotlin/manualTests.kt
+++ kotlin-parser/src/test/kotlin/manualTests.kt
@@ -13,20 +13,18 @@ import java.math.BigInteger
 class JUnitTests {
     @Test
     fun runPythagoreanTripleFunction1() {
-        val function = tokenize("src/test/semlang/pythagoreanTriple1.sem").first()
-        val functionsMap = mapOf(function.id to function)
-        val interpreter = SemlangForwardInterpreter(functionsMap)
-        val result123 = interpreter.interpret(function.id, listOf(int(1), int(2), int(3)))
-        assertEquals(SemObject.Boolean(false), result123)
-        val result345 = interpreter.interpret(function.id, listOf(int(3), int(4), int(5)))
-        assertEquals(SemObject.Boolean(true), result345)
+        testPythagoreanTripleFunction("src/test/semlang/pythagoreanTriple1.sem")
     }
 
     @Test
     fun runPythagoreanTripleFunction2() {
-        val functions = tokenize("src/test/semlang/pythagoreanTriple2.sem")
+        // Tests the use of an additional function
+        testPythagoreanTripleFunction("src/test/semlang/pythagoreanTriple2.sem")
+    }
+
+    private fun testPythagoreanTripleFunction(filename: String) {
+        val functions = tokenize(filename)
         val functionsMap = mapById(functions)
-        System.out.println(functionsMap)
         val mainFunctionId = FunctionId(Package(listOf()), "pythagoreanTripleCheck")
         val interpreter = SemlangForwardInterpreter(functionsMap)
         val result123 = interpreter.interpret(mainFunctionId, listOf(int(1), int(2), int(3)))
