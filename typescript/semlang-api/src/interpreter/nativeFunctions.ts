cd48db03f18b75a9ac187b52afa6f572dfeacb29
AlexLandau <alandau@cs.stanford.edu>
2018-01-21 16:51:57 -0800

    Add the Sequence-related native elements

--- typescript/semlang-api/src/interpreter/nativeFunctions.ts
+++ typescript/semlang-api/src/interpreter/nativeFunctions.ts
@@ -1,10 +1,11 @@
 import * as UtfString from "utfstring";
-import { SemObject, integerObject, booleanObject, naturalObject, listObject, failureObject, successObject } from "./SemObject";
-import { Struct } from "../api/language";
+import { SemObject, integerObject, booleanObject, naturalObject, listObject, failureObject, successObject, instanceObject, structObject, bindingObject } from "./SemObject";
+import { Struct, Type, Interface } from "../api/language";
 import { InterpreterContext } from "./interpret";
 
 
 // TODO: Would be nice to simplify further by offering these as parseable, or linked into sem0...
+const typeT: Type.NamedType = { name: "T" };
 export const NativeStructs: { [structName: string]: Struct } = {
     "Unicode.CodePoint": {
         id: "Unicode.CodePoint",
@@ -23,9 +24,59 @@ export const NativeStructs: { [structName: string]: Struct } = {
             }
         ],
     },
+    "BasicSequence": {
+        id: "BasicSequence",
+        typeParameters: ['T'],
+        members: [
+            { name: "base", type: typeT },
+            { name: "successor", type: { from: [typeT], to: typeT } },
+        ],
+    },
+};
+
+export const NativeInterfaces: { [interfaceName: string]: Interface } = {
+    "Sequence": {
+        id: "Sequence",
+        typeParameters: ['T'],
+        methods: [
+            { name: "get", arguments: [{name: "index", type: "Natural"}], returnType: typeT },
+            { name: "first", arguments: [{name: "condition", type: { from: [typeT], to: "Boolean" }}], returnType: typeT },
+        ],
+    },
 };
 
 export const NativeFunctions: { [functionName: string]: Function } = {
+    "BasicSequence.first": (context: InterpreterContext, basicSequence: SemObject.Struct, predicate: SemObject.FunctionBinding): SemObject => {
+        const base = basicSequence.members[0];
+        const successor = basicSequence.members[1];
+        if (successor.type !== "binding") {
+            throw new Error(`Expected a BasicSequence successor to be a function binding`);
+        }
+        let curValue: SemObject = base;
+        while (true) {
+            const isSatisfying = context.evaluateBoundFunction(predicate, [curValue]);
+            if (isSatisfying.type !== "Boolean") {
+                throw new Error(`Expected boolean output from a Sequence.first predicate, but was ${JSON.stringify(isSatisfying)}`);
+            }
+            if (isSatisfying.value) {
+                return curValue;
+            } else {
+                curValue = context.evaluateBoundFunction(successor, [curValue]);
+            }
+        }
+    },
+    "BasicSequence.get": (context: InterpreterContext, basicSequence: SemObject.Struct, index: SemObject.Natural): SemObject => {
+        const base = basicSequence.members[0];
+        const successor = basicSequence.members[1];
+        if (successor.type !== "binding") {
+            throw new Error(`Expected a BasicSequence successor to be a function binding`);
+        }
+        let curValue: SemObject = base;
+        for (let i = 0; i < index.value; i++) {
+            curValue = context.evaluateBoundFunction(successor, [curValue]);
+        }
+        return curValue;
+    },
     "Boolean.and": (context: InterpreterContext, a: SemObject.Boolean, b: SemObject.Boolean): SemObject.Boolean => {
         return booleanObject(a.value && b.value);
     },
@@ -67,11 +118,20 @@ export const NativeFunctions: { [functionName: string]: Function } = {
     "List.size": (context: InterpreterContext, list: SemObject.List): SemObject.Natural => {
         return naturalObject(list.contents.length);
     },
+    "Natural.absoluteDifference": (context: InterpreterContext, left: SemObject.Natural, right: SemObject.Natural): SemObject.Natural => {
+        const difference = left.value - right.value;
+        return naturalObject((difference >= 0) ? difference : -difference);
+    },
     "Natural.equals": (context: InterpreterContext, left: SemObject.Natural, right: SemObject.Natural): SemObject.Boolean => {
         return booleanObject(left.value === right.value);
     },
     "Natural.greaterThan": (context: InterpreterContext, left: SemObject.Natural, right: SemObject.Natural): SemObject.Boolean => {
         return booleanObject(left.value > right.value);
+    },
+    "Natural.lesser": (context: InterpreterContext, left: SemObject.Natural, right: SemObject.Natural): SemObject.Natural => {
+        const a = left.value;
+        const b = right.value;
+        return naturalObject((a < b) ? a : b);
     },
     "Natural.lessThan": (context: InterpreterContext, left: SemObject.Natural, right: SemObject.Natural): SemObject.Boolean => {
         return booleanObject(left.value < right.value);
@@ -81,6 +141,20 @@ export const NativeFunctions: { [functionName: string]: Function } = {
     },
     "Natural.times": (context: InterpreterContext, left: SemObject.Natural, right: SemObject.Natural): SemObject.Natural => {
         return naturalObject(left.value * right.value);
+    },
+    "Sequence.create": (context: InterpreterContext, base: SemObject, successor: SemObject.FunctionBinding): SemObject => {
+        const sequenceInterface = NativeInterfaces["Sequence"];
+        const dataObject = structObject(NativeStructs["BasicSequence"], [
+            base,
+            successor
+        ]);
+
+        const boundMethods = [
+            bindingObject("BasicSequence.get", [dataObject, undefined]),
+            bindingObject("BasicSequence.first", [dataObject, undefined]),
+        ];
+
+        return instanceObject(sequenceInterface, boundMethods);
     },
     "Try.assume": (context: InterpreterContext, theTry: SemObject.Try): SemObject => {
         if (theTry.type === "Try.Success") {
