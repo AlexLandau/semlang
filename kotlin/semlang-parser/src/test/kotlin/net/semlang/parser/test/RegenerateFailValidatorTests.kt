10a2f391b37f839d7af6e10e1e8307b19cbbe1a0
Alex Landau <alandau@cs.stanford.edu>
2018-11-21 14:29:46 -0800

    Add script/task to regenerate failValidator .errors test files

---
+++ kotlin/semlang-parser/src/test/kotlin/net/semlang/parser/test/RegenerateFailValidatorTests.kt
@@ -0,0 +1,32 @@
+package net.semlang.parser.test
+
+import net.semlang.api.CURRENT_NATIVE_MODULE_VERSION
+import net.semlang.api.ModuleName
+import net.semlang.parser.ParsingResult
+import net.semlang.parser.parseString
+import net.semlang.validator.ValidationResult
+import net.semlang.validator.validate
+import java.io.File
+
+private val TEST_MODULE_NAME = ModuleName("semlang", "validatorTestFile")
+
+fun main() {
+    for (file in File("src/test/semlang/validatorTests/failValidator").listFiles()) {
+        if (!file.endsWith(".errors")) {
+            continue
+        }
+
+        val errorFile = loadErrorFile(file)
+        val text = errorFile.getText()
+        val parsingResult = parseString(text, file.absolutePath)
+        if (parsingResult !is ParsingResult.Success) {
+            error("Cannot regenerate the failValidator files: File $file has a parsing error rather than a validation error")
+        }
+        val validationResult = validate(parsingResult, TEST_MODULE_NAME, CURRENT_NATIVE_MODULE_VERSION, listOf())
+        if (validationResult !is ValidationResult.Failure) {
+            error("Cannot regenerate the failValidator files: File $file has no reported validation errors")
+        }
+        val regeneratedErrorFile = ErrorFile(errorFile.lines, validationResult.errors.toSet())
+        file.writeText(regeneratedErrorFile.getText())
+    }
+}
